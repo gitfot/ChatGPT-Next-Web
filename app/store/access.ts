4a666c05472828d3ce2f1c08219d25e22b364ca6
zs-zhanglvxiu <ptowan@163.com>
2023-09-04 15:24:41 +0800

    update

--- app/store/access.ts
+++ app/store/access.ts
@@ -1,100 +1,103 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { DEFAULT_API_HOST, DEFAULT_MODELS, StoreKey } from "../constant";
-import { getHeaders } from "../client/api";
-import { getClientConfig } from "../config/client";
+import {create} from "zustand";
+import {persist} from "zustand/middleware";
+import {DEFAULT_API_HOST, DEFAULT_MODELS, StoreKey} from "../constant";
+import {getHeaders} from "../client/api";
+import {getClientConfig} from "../config/client";
 
 export interface AccessControlStore {
-  accessCode: string;
-  token: string;
+	accessCode: string;
+	token: string;
 
-  needCode: boolean;
-  hideUserApiKey: boolean;
-  hideBalanceQuery: boolean;
-  disableGPT4: boolean;
+	needCode: boolean;
+	hideUserApiKey: boolean;
+	hideBalanceQuery: boolean;
+	disableGPT4: boolean;
 
-  openaiUrl: string;
+	openaiUrl: string;
 
-  updateToken: (_: string) => void;
-  updateCode: (_: string) => void;
-  updateOpenAiUrl: (_: string) => void;
-  enabledAccessControl: () => boolean;
-  isAuthorized: () => boolean;
-  fetch: () => void;
+	updateToken: (_: string) => void;
+	updateCode: (_: string) => void;
+	updateOpenAiUrl: (_: string) => void;
+	enabledAccessControl: () => boolean;
+	isAuthorized: () => boolean;
+	fetch: () => void;
 }
 
 let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 
 const DEFAULT_OPENAI_URL =
-  getClientConfig()?.buildMode === "export" ? DEFAULT_API_HOST : "/api/openai/";
+	getClientConfig()?.buildMode === "export" ? DEFAULT_API_HOST : "/api/openai/";
 console.log("[API] default openai url", DEFAULT_OPENAI_URL);
 
+/**
+ *
+ */
 export const useAccessStore = create<AccessControlStore>()(
-  persist(
-    (set, get) => ({
-      token: "",
-      accessCode: "",
-      needCode: true,
-      hideUserApiKey: false,
-      hideBalanceQuery: false,
-      disableGPT4: false,
+	persist(
+		(set, get) => ({
+			token: "",
+			accessCode: "",
+			needCode: true,
+			hideUserApiKey: false,
+			hideBalanceQuery: false,
+			disableGPT4: false,
 
-      openaiUrl: DEFAULT_OPENAI_URL,
+			openaiUrl: DEFAULT_OPENAI_URL,
 
-      enabledAccessControl() {
-        get().fetch();
-        return get().needCode;
-      },
-      updateCode(code: string) {
-        set(() => ({ accessCode: code?.trim() }));
-      },
-      updateToken(token: string) {
-        set(() => ({ token: token?.trim() }));
-      },
-      updateOpenAiUrl(url: string) {
-        set(() => ({ openaiUrl: url?.trim() }));
-      },
-      isAuthorized() {
-        get().fetch();
+			enabledAccessControl() {
+				get().fetch();
+				return get().needCode;
+			},
+			updateCode(code: string) {
+				set(() => ({accessCode: code?.trim()}));
+			},
+			updateToken(token: string) {
+				set(() => ({token: token?.trim()}));
+			},
+			updateOpenAiUrl(url: string) {
+				set(() => ({openaiUrl: url?.trim()}));
+			},
+			isAuthorized() {
+				get().fetch();
 
-        // has token or has code or disabled access control
-        return (
-          !!get().token || !!get().accessCode || !get().enabledAccessControl()
-        );
-      },
-      fetch() {
-        if (fetchState > 0 || getClientConfig()?.buildMode === "export") return;
-        fetchState = 1;
-        fetch("/api/config", {
-          method: "post",
-          body: null,
-          headers: {
-            ...getHeaders(),
-          },
-        })
-          .then((res) => res.json())
-          .then((res: DangerConfig) => {
-            console.log("[Config] got config from server", res);
-            set(() => ({ ...res }));
+				// has token or has code or disabled access control
+				return (
+					!!get().token || !!get().accessCode || !get().enabledAccessControl()
+				);
+			},
+			fetch() {
+				if (fetchState > 0 || getClientConfig()?.buildMode === "export") return;
+				fetchState = 1;
+				fetch("/api/config", {
+					method: "post",
+					body: null,
+					headers: {
+						...getHeaders(),
+					},
+				})
+					.then((res) => res.json())
+					.then((res: DangerConfig) => {
+						console.log("[Config] got config from server", res);
+						set(() => ({...res}));
 
-            if (res.disableGPT4) {
-              DEFAULT_MODELS.forEach(
-                //修改模型是否可用
-                (m: any) => (m.available = !m.name.startsWith("gpt-4")),
-              );
-            }
-          })
-          .catch(() => {
-            console.error("[Config] failed to fetch config");
-          })
-          .finally(() => {
-            fetchState = 2;
-          });
-      },
-    }),
-    {
-      name: StoreKey.Access,
-      version: 1,
-    },
-  ),
+						if (res.disableGPT4) {
+							DEFAULT_MODELS.forEach(
+								//修改模型是否可用
+								(m: any) => (m.available = !m.name.startsWith("gpt-4")),
+							);
+						}
+					})
+					.catch(() => {
+						console.error("[Config] failed to fetch config");
+					})
+					.finally(() => {
+						fetchState = 2;
+					});
+			},
+		}),
+		{
+			name: StoreKey.Access,
+			version: 1,
+		},
+	),
 );
