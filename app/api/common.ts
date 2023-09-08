703e9a5a1510e9a651ae7a9eb8b9a17f12b87e06
zs-zhanglvxiu <ptowan@163.com>
2023-09-08 10:23:13 +0800

    update

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
 export const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
@@ -7,92 +7,118 @@ const BASE_URL = process.env.BASE_URL || OPENAI_URL;
 const DISABLE_GPT4 = !!process.env.DISABLE_GPT4;
 
 export async function requestOpenai(req: NextRequest) {
-  const controller = new AbortController();
-  const authValue = req.headers.get("Authorization") ?? "";
-  const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
-    "/api/openai/",
-    "",
-  );
+	const controller = new AbortController();
+	const authValue = req.headers.get("Authorization") ?? "";
+	const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
+		// "/api/openai/",
+		"/api/",
+		"",
+	);
 
-  let baseUrl = BASE_URL;
+	let baseUrl = BASE_URL;
 
-  if (!baseUrl.startsWith("http")) {
-    baseUrl = `${PROTOCOL}://${baseUrl}`;
-  }
+	if (!baseUrl.startsWith("http")) {
+		baseUrl = `${PROTOCOL}://${baseUrl}`;
+	}
 
-  if (baseUrl.endsWith('/')) {
-    baseUrl = baseUrl.slice(0, -1);
-  }
+	if (baseUrl.endsWith('/')) {
+		baseUrl = baseUrl.slice(0, -1);
+	}
 
-  console.log("[Proxy] ", openaiPath);
-  console.log("[Base Url]", baseUrl);
+	console.log("[Proxy] ", openaiPath);
+	console.log("[Base Url]", baseUrl);
 
-  if (process.env.OPENAI_ORG_ID) {
-    console.log("[Org ID]", process.env.OPENAI_ORG_ID);
-  }
+	if (process.env.OPENAI_ORG_ID) {
+		console.log("[Org ID]", process.env.OPENAI_ORG_ID);
+	}
 
-  const timeoutId = setTimeout(() => {
-    controller.abort();
-  }, 10 * 60 * 1000);
+	const timeoutId = setTimeout(() => {
+		controller.abort();
+	}, 10 * 60 * 1000);
 
-  const fetchUrl = `${baseUrl}/${openaiPath}`;
-  const fetchOptions: RequestInit = {
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store",
-      Authorization: authValue,
-      ...(process.env.OPENAI_ORG_ID && {
-        "OpenAI-Organization": process.env.OPENAI_ORG_ID,
-      }),
-    },
-    method: req.method,
-    body: req.body,
-    // to fix #2485: https://stackoverflow.com/questions/55920957/cloudflare-worker-typeerror-one-time-use-body
-    redirect: "manual",
-    // @ts-ignore
-    duplex: "half",
-    signal: controller.signal,
-  };
+	const fetchUrl = `${baseUrl}/${openaiPath}`;
+	const fetchOptions: RequestInit = {
+		headers: {
+			"Content-Type": "application/json",
+			"Cache-Control": "no-store",
+			Authorization: authValue,
+			...(process.env.OPENAI_ORG_ID && {
+				"OpenAI-Organization": process.env.OPENAI_ORG_ID,
+			}),
+		},
+		method: req.method,
+		body: req.body,
+		// to fix #2485: https://stackoverflow.com/questions/55920957/cloudflare-worker-typeerror-one-time-use-body
+		redirect: "manual",
+		// @ts-ignore
+		duplex: "half",
+		signal: controller.signal,
+	};
 
-  // #1815 try to refuse gpt4 request
-  if (DISABLE_GPT4 && req.body) {
-    try {
-      const clonedBody = await req.text();
-      fetchOptions.body = clonedBody;
+	try {
+		const res = await fetch(fetchUrl, fetchOptions);
 
-      const jsonBody = JSON.parse(clonedBody);
+		// to prevent browser prompt for credentials
+		const newHeaders = new Headers(res.headers);
+		newHeaders.delete("www-authenticate");
+		// to disable nginx buffering
+		newHeaders.set("X-Accel-Buffering", "no");
 
-      if ((jsonBody?.model ?? "").includes("gpt-4")) {
-        return NextResponse.json(
-          {
-            error: true,
-            message: "you are not allowed to use gpt-4 model",
-          },
-          {
-            status: 403,
-          },
-        );
-      }
-    } catch (e) {
-      console.error("[OpenAI] gpt4 filter", e);
-    }
-  }
+		return new Response(res.body, {
+			status: res.status,
+			statusText: res.statusText,
+			headers: newHeaders,
+		});
+	} finally {
+		clearTimeout(timeoutId);
+	}
+}
 
-  try {
-    const res = await fetch(fetchUrl, fetchOptions);
+export async function request(req: NextRequest) {
+	const controller = new AbortController();
+	let baseUrl = BASE_URL;
 
-    // to prevent browser prompt for credentials
-    const newHeaders = new Headers(res.headers);
-    newHeaders.delete("www-authenticate");
-    // to disable nginx buffering
-    newHeaders.set("X-Accel-Buffering", "no");
+	if (!baseUrl.startsWith("http")) {
+		baseUrl = `${PROTOCOL}://${baseUrl}`;
+	}
+	const authValue = req.headers.get("Authorization") ?? "";
+	const uri = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
+		"/api/",
+		"",
+	);
 
-    return new Response(res.body, {
-      status: res.status,
-      statusText: res.statusText,
-      headers: newHeaders,
-    });
-  } finally {
-    clearTimeout(timeoutId);
-  }
+	const timeoutId = setTimeout(() => {
+		controller.abort();
+	}, 10 * 60 * 1000);
+
+	try {
+		console.log(`url = ${baseUrl}/${uri}`);
+		const res = await fetch(`${baseUrl}/${uri}`, {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: authValue,
+			},
+			cache: "no-store",
+			method: req.method,
+			body: req.body,
+			// @ts-ignore
+			duplex: "half",
+			signal: controller.signal,
+		});
+
+		// to prevent browser prompt for credentials
+		const newHeaders = new Headers(res.headers);
+		newHeaders.delete("www-authenticate");
+
+		// to disbale ngnix buffering
+		newHeaders.set("X-Accel-Buffering", "no");
+
+		return new Response(res.body, {
+			status: res.status,
+			statusText: res.statusText,
+			headers: newHeaders,
+		});
+	} finally {
+		clearTimeout(timeoutId);
+	}
 }
