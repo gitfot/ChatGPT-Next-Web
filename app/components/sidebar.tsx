09a0c166728380b7348ef07702381ea66b5ff2ea
zs-zhanglvxiu <p2wan@163.com>
2023-09-29 02:49:13 +0800

    reindent

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -16,10 +16,10 @@ import Locale from "../locales";
 import { useAppConfig, useChatStore } from "../store";
 
 import {
-  MAX_SIDEBAR_WIDTH,
-  MIN_SIDEBAR_WIDTH,
-  NARROW_SIDEBAR_WIDTH,
-  Path,
+    MAX_SIDEBAR_WIDTH,
+    MIN_SIDEBAR_WIDTH,
+    NARROW_SIDEBAR_WIDTH,
+    Path,
 } from "../constant";
 
 import { Link, useNavigate } from "react-router-dom";
@@ -29,181 +29,192 @@ import { showConfirm, showToast } from "./ui-lib";
 import ProfileConfig from "@/app/components/profile-config";
 
 const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
-  loading: () => null,
+    loading: () => null,
 });
 
 function useHotKey() {
-  const chatStore = useChatStore();
+    const chatStore = useChatStore();
 
-  useEffect(() => {
-    const onKeyDown = (e: KeyboardEvent) => {
-      if (e.altKey || e.ctrlKey) {
-        if (e.key === "ArrowUp") {
-          chatStore.nextSession(-1);
-        } else if (e.key === "ArrowDown") {
-          chatStore.nextSession(1);
-        }
-      }
-    };
+    useEffect(() => {
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.altKey || e.ctrlKey) {
+                if (e.key === "ArrowUp") {
+                    chatStore.nextSession(-1);
+                } else if (e.key === "ArrowDown") {
+                    chatStore.nextSession(1);
+                }
+            }
+        };
 
-    window.addEventListener("keydown", onKeyDown);
-    return () => window.removeEventListener("keydown", onKeyDown);
-  });
+        window.addEventListener("keydown", onKeyDown);
+        return () => window.removeEventListener("keydown", onKeyDown);
+    });
 }
 
 function useDragSideBar() {
-  const limit = (x: number) => Math.min(MAX_SIDEBAR_WIDTH, x);
+    const limit = (x: number) => Math.min(MAX_SIDEBAR_WIDTH, x);
 
-  const config = useAppConfig();
-  const startX = useRef(0);
-  const startDragWidth = useRef(config.sidebarWidth ?? 300);
-  const lastUpdateTime = useRef(Date.now());
+    const config = useAppConfig();
+    const startX = useRef(0);
+    const startDragWidth = useRef(config.sidebarWidth ?? 300);
+    const lastUpdateTime = useRef(Date.now());
 
-  const handleMouseMove = useRef((e: MouseEvent) => {
-    if (Date.now() < lastUpdateTime.current + 50) {
-      return;
-    }
-    lastUpdateTime.current = Date.now();
-    const d = e.clientX - startX.current;
-    const nextWidth = limit(startDragWidth.current + d);
-    config.update((config) => (config.sidebarWidth = nextWidth));
-  });
+    const handleMouseMove = useRef((e: MouseEvent) => {
+        if (Date.now() < lastUpdateTime.current + 50) {
+            return;
+        }
+        lastUpdateTime.current = Date.now();
+        const d = e.clientX - startX.current;
+        const nextWidth = limit(startDragWidth.current + d);
+        config.update((config) => (config.sidebarWidth = nextWidth));
+    });
 
-  const handleMouseUp = useRef(() => {
-    startDragWidth.current = config.sidebarWidth ?? 300;
-    window.removeEventListener("mousemove", handleMouseMove.current);
-    window.removeEventListener("mouseup", handleMouseUp.current);
-  });
+    const handleMouseUp = useRef(() => {
+        startDragWidth.current = config.sidebarWidth ?? 300;
+        window.removeEventListener("mousemove", handleMouseMove.current);
+        window.removeEventListener("mouseup", handleMouseUp.current);
+    });
 
-  const onDragMouseDown = (e: MouseEvent) => {
-    startX.current = e.clientX;
+    const onDragMouseDown = (e: MouseEvent) => {
+        startX.current = e.clientX;
 
-    window.addEventListener("mousemove", handleMouseMove.current);
-    window.addEventListener("mouseup", handleMouseUp.current);
-  };
-  const isMobileScreen = useMobileScreen();
-  const shouldNarrow =
-    !isMobileScreen && config.sidebarWidth < MIN_SIDEBAR_WIDTH;
+        window.addEventListener("mousemove", handleMouseMove.current);
+        window.addEventListener("mouseup", handleMouseUp.current);
+    };
+    const isMobileScreen = useMobileScreen();
+    const shouldNarrow =
+        !isMobileScreen && config.sidebarWidth < MIN_SIDEBAR_WIDTH;
 
-  useEffect(() => {
-    const barWidth = shouldNarrow
-      ? NARROW_SIDEBAR_WIDTH
-      : limit(config.sidebarWidth ?? 300);
-    const sideBarWidth = isMobileScreen ? "100vw" : `${barWidth}px`;
-    document.documentElement.style.setProperty("--sidebar-width", sideBarWidth);
-  }, [config.sidebarWidth, isMobileScreen, shouldNarrow]);
+    useEffect(() => {
+        const barWidth = shouldNarrow
+            ? NARROW_SIDEBAR_WIDTH
+            : limit(config.sidebarWidth ?? 300);
+        const sideBarWidth = isMobileScreen ? "100vw" : `${barWidth}px`;
+        document.documentElement.style.setProperty(
+            "--sidebar-width",
+            sideBarWidth,
+        );
+    }, [config.sidebarWidth, isMobileScreen, shouldNarrow]);
 
-  return {
-    onDragMouseDown,
-    shouldNarrow,
-  };
+    return {
+        onDragMouseDown,
+        shouldNarrow,
+    };
 }
 
 export function SideBar(props: { className?: string }) {
-  const chatStore = useChatStore();
+    const chatStore = useChatStore();
 
-  // drag side bar
-  const { onDragMouseDown, shouldNarrow } = useDragSideBar();
-  const navigate = useNavigate();
-  const config = useAppConfig();
+    // drag side bar
+    const { onDragMouseDown, shouldNarrow } = useDragSideBar();
+    const navigate = useNavigate();
+    const config = useAppConfig();
 
-  useHotKey();
+    useHotKey();
 
-  return (
-    <div
-      className={`${styles.sidebar} ${props.className} ${
-        shouldNarrow && styles["narrow-sidebar"]
-      }`}
-    >
-      <div className={styles["sidebar-header"]} data-tauri-drag-region>
-        <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+    return (
+        <div
+            className={`${styles.sidebar} ${props.className} ${
+                shouldNarrow && styles["narrow-sidebar"]
+            }`}
+        >
+            <div className={styles["sidebar-header"]} data-tauri-drag-region>
+                <div className={styles["sidebar-title"]} data-tauri-drag-region>
+                    ChatGPT Next
+                </div>
+                <div className={styles["sidebar-sub-title"]}>
+                    Build your own AI assistant.
+                </div>
+                <div className={styles["sidebar-logo"] + " no-dark"}>
+                    <ChatGptIcon />
+                </div>
+            </div>
+
+            <div className={styles["sidebar-header-bar"]}>
+                <IconButton
+                    icon={<MaskIcon />}
+                    text={shouldNarrow ? undefined : Locale.Mask.Name}
+                    className={styles["sidebar-bar-button"]}
+                    onClick={() =>
+                        navigate(Path.NewChat, { state: { fromHome: true } })
+                    }
+                    shadow
+                />
+                <IconButton
+                    icon={<PluginIcon />}
+                    text={shouldNarrow ? undefined : Locale.Plugin.Name}
+                    className={styles["sidebar-bar-button"]}
+                    onClick={() => showToast(Locale.WIP)}
+                    shadow
+                />
+            </div>
+
+            <div className={styles["sidebar-new-chat-button"]}>
+                <IconButton
+                    icon={<AddIcon />}
+                    text={shouldNarrow ? undefined : Locale.Home.NewChat}
+                    onClick={() => {
+                        if (config.dontShowMaskSplashScreen) {
+                            chatStore.newSession();
+                            navigate(Path.Chat);
+                        } else {
+                            navigate(Path.NewChat);
+                        }
+                    }}
+                    shadow
+                />
+            </div>
+
+            <div
+                className={styles["sidebar-body"]}
+                onClick={(e) => {
+                    if (e.target === e.currentTarget) {
+                        navigate(Path.Home);
+                    }
+                }}
+            >
+                <p className={styles["sidebar-body-title"]}>Chats</p>
+
+                <ChatList narrow={shouldNarrow} />
+            </div>
+
+            <div className={styles["sidebar-tail"]}>
+                <div className={styles["sidebar-actions"]}>
+                    <div
+                        className={
+                            styles["sidebar-action"] + " " + styles.mobile
+                        }
+                    >
+                        <IconButton
+                            icon={<CloseIcon />}
+                            onClick={async () => {
+                                if (await showConfirm(Locale.Home.DeleteChat)) {
+                                    chatStore.deleteSession(
+                                        chatStore.currentSessionIndex,
+                                    );
+                                }
+                            }}
+                        />
+                    </div>
+
+                    <div className={styles["sidebar-action"]}>
+                        <ProfileConfig />
+                    </div>
+
+                    <div className={styles["sidebar-action"]}>
+                        <Link to={Path.Settings}>
+                            <IconButton icon={<SettingsIcon />} shadow />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <div
+                className={styles["sidebar-drag"]}
+                onMouseDown={(e) => onDragMouseDown(e as any)}
+            >
+                <DragIcon />
+            </div>
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
-        <div className={styles["sidebar-logo"] + " no-dark"}>
-          <ChatGptIcon />
-        </div>
-      </div>
-
-      <div className={styles["sidebar-header-bar"]}>
-        <IconButton
-          icon={<MaskIcon />}
-          text={shouldNarrow ? undefined : Locale.Mask.Name}
-          className={styles["sidebar-bar-button"]}
-          onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
-          shadow
-        />
-        <IconButton
-          icon={<PluginIcon />}
-          text={shouldNarrow ? undefined : Locale.Plugin.Name}
-          className={styles["sidebar-bar-button"]}
-          onClick={() => showToast(Locale.WIP)}
-          shadow
-        />
-      </div>
-
-      <div className={styles["sidebar-new-chat-button"]}>
-        <IconButton
-          icon={<AddIcon />}
-          text={shouldNarrow ? undefined : Locale.Home.NewChat}
-          onClick={() => {
-            if (config.dontShowMaskSplashScreen) {
-              chatStore.newSession();
-              navigate(Path.Chat);
-            } else {
-              navigate(Path.NewChat);
-            }
-          }}
-          shadow
-        />
-      </div>
-
-      <div
-        className={styles["sidebar-body"]}
-        onClick={(e) => {
-          if (e.target === e.currentTarget) {
-            navigate(Path.Home);
-          }
-        }}
-      >
-        <p className={styles["sidebar-body-title"]}>Chats</p>
-
-        <ChatList narrow={shouldNarrow} />
-      </div>
-
-      <div className={styles["sidebar-tail"]}>
-        <div className={styles["sidebar-actions"]}>
-          <div className={styles["sidebar-action"] + " " + styles.mobile}>
-            <IconButton
-              icon={<CloseIcon />}
-              onClick={async () => {
-                if (await showConfirm(Locale.Home.DeleteChat)) {
-                  chatStore.deleteSession(chatStore.currentSessionIndex);
-                }
-              }}
-            />
-          </div>
-
-          <div className={styles["sidebar-action"]}>
-            <ProfileConfig />
-          </div>
-
-          <div className={styles["sidebar-action"]}>
-            <Link to={Path.Settings}>
-              <IconButton icon={<SettingsIcon />} shadow />
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      <div
-        className={styles["sidebar-drag"]}
-        onMouseDown={(e) => onDragMouseDown(e as any)}
-      >
-        <DragIcon />
-      </div>
-    </div>
-  );
+    );
 }
